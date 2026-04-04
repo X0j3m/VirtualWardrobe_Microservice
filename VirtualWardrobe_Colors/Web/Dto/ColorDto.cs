3e96b2e02417e880e14b21df3fcc3f01a6d9c591
Michał Ptasznik <michal.ptasznik@protonmail.com>
2026-04-04 18:34:17 +0200

    Updated folder structure

--- VirtualWardrobe_Colors/Web/Dto/ColorDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VirtualWardrobe_Colors.Web.Dto
-{
-    public record ColorDto(Guid Id, string Name);
-
-    public record CreateColorDto(string Name);
-
-    public record UpdateColorDto(string Name);
-}
